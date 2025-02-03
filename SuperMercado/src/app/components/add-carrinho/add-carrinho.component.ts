2e4ebf2006f5a20d2ba2c7fd1c09d6ecebaafe6d
Leandro <lesizilio2gmail.com>
2025-02-02 21:12:55 -0300

    feat: Base da aplicação

---
+++ SuperMercado/src/app/components/add-carrinho/add-carrinho.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-carrinho',
+  imports: [],
+  templateUrl: './add-carrinho.component.html',
+  styleUrl: './add-carrinho.component.css'
+})
+export class AddCarrinhoComponent {
+
+}
